42c12493fcb8bc6ce31bfaea458b556c23783318
danpeterson1128 <118423776+danpeterson1128@users.noreply.github.com>
2022-11-23 10:59:43 +0800

    Add files via upload

---
+++ greeting.py
@@ -0,0 +1,16 @@
+#hello = "Hello"
+#name = input("What's your name?\n")
+#greeting = hello + " " + name
+#print(greeting)
+
+def greeting(name):
+    print('Hello', name)
+
+
+name1 = input('Enter your name:\n')
+greeting(name1)
+name2 = input('Enter another name:\n')
+greeting(name2)
+
+
+
---
+++ weather.py
@@ -0,0 +1,25 @@
+import pip._vendor.requests
+
+
+def get_weather_desc_and_temp():
+    api_key = "67da29cb91129f1a68c1c06c1be92daa"
+    city = "Orlando"
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
+    request = pip._vendor.requests.get(url)
+    json = request.json()
+    description = json.get("weather")[0].get("description")
+    temp_min = json.get("main").get("temp_min")
+    temp_max = json.get("main").get("temp_max")
+
+    return {'description':description,
+             'temp_min':temp_min,
+             'temp_max':temp_max}
+
+def main():
+    weather_dict = get_weather_desc_and_temp()
+    #print the results
+    print("Today's forecast is", weather_dict.get('description'))
+    print("The minimum temperature is", weather_dict.get('temp_min'))
+    print("The maximum tempterature is", weather_dict.get('temp_max'))
+
+main()
